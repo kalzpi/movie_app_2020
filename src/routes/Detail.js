9d18006e0302c8b178be43b10daeb0a212f8664e
kalzpi <48274440+kalzpi@users.noreply.github.com>
2020-01-06 20:45:31 +0900

    Everything Done!

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,21 @@
 import React from "react";
 
-function Detail({ location }) {
-  console.log(location);
-  return <span>Detail</span>;
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    console.log(location);
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
